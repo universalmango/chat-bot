3bcb09d3cae0c810ee29fd7fec328afad519bf12
universalmango <143958790+universalmango@users.noreply.github.com>
2023-09-06 23:20:09 +0000

    made the outline of what the chat bot will be like

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
-print('hello there!')
-print('what would you like to talk about today?')
+print('Hello there welcome to the customer service chatbot')
+print('   ')
+user_name = input('What is your name? ')
+user_age = input('What is your age? ')
+print('  ')
+print('How may I help you.')
+
+def menu():
+  print('1: place holder')
+  print('2: place holder')
+  print('3: place holder')
+  print('4: place holder')
+  print('0: exit')
+
+menu()
+user_choice = int(input('please select the field that pertains to you'))
+
+while user_choice != 0:
+  if user_choice == 1:
+    print()
+    print('1')
+  elif user_choice == 2:
+    print()
+    print('2')
+  elif user_choice == 3:
+    print()
+    print('3')
+  elif user_choice == 4:
+    print()
+    print('4')
+  else:
+    print()
+    print('invalid option')
+
+  print()
+  menu()
+  user_choice = int(input('please select the field that pertains to you'))
+
+print('Thank you for  using customer service chatbot!')
